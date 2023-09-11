c423ed863aa2c63c2e0fc1c0e3aa23d63b6fd5d4
Marcus Reaiche <marcusreaiche@gmail.com>
2023-09-11 13:48:10 -0400

    Solve use_artifact

--- lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
@@ -18,24 +18,27 @@ def go(args):
 
     # YOUR CODE HERE: get the artifact and store its local path in the variable "artifact_path"
     # HINT: you can get the artifact path by using the "file()" method
-
+    artifact = run.use_artifact(args.artifact_name)
     artifact_path = artifact.file()
 
     logger.info("Artifact content:")
+    logger.info(artifact_path)
     with open(artifact_path, "r") as fp:
         content = fp.read()
 
     print(content)
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser(
-        description="Use an artifact from W&B", fromfile_prefix_chars="@"
-    )
+        description="Use an artifact from W&B",
+        fromfile_prefix_chars="@")
 
     parser.add_argument(
-        "--artifact_name", type=str, help="Name and version of W&B artifact", required=True
-    )
+        "--artifact_name",
+        type=str,
+        help="Name and version of W&B artifact",
+        required=True)
 
     args = parser.parse_args()
 
